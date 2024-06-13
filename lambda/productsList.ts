e97e82a41b86a48f69f41db6a211f8134737b3d1
OksanaFedotova <oxana-fedotova@yandex.ru>
2024-06-13 20:23:43 +0400

    feat: add lambda for product lais

---
+++ lambda/productsList.ts
@@ -0,0 +1,14 @@
+import {  APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
+import { IProduct, products } from "./data";
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json",
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+         },
+        body: JSON.stringify(products),
+    };
+};
--- test/back.test.ts
+++ test/back.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as RsAwsCdkBack from '../lib/rs-aws-cdk-back-stack';
+// import * as Back from '../lib/back-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/rs-aws-cdk-back-stack.ts
+// example resource in lib/back-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new RsAwsCdkBack.RsAwsCdkBackStack(app, 'MyTestStack');
+//   const stack = new Back.BackStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
