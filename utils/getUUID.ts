3ca8d27546f0f8efaf2b8d094763c48784277ceb
OksanaFedotova <oxana-fedotova@yandex.ru>
2024-06-19 17:19:17 +0400

    feat: add script to put items in dynamo db

---
+++ utils/getUUID.ts
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// Функция для генерации UUID
+function generateUUID(): string {
+    return uuidv4();
+}
+
+// Пример использования
+const uuid: string = generateUUID();
+console.log('Generated UUID:', uuid);
