4e0d675516127e555400b0e0f851462f63e2ce2a
OksanaFedotova <oxana-fedotova@yandex.ru>
2024-06-28 12:49:19 +0400

    feat: tests implemented

--- import-service/lambda/importFileParser.ts
+++ import-service/lambda/importFileParser.ts
@@ -1,12 +1,13 @@
-import * as AWS from 'aws-sdk';
-import * as csvParser from 'csv-parser';
-import * as stream from 'stream';
+import * as AWS from "aws-sdk";
+//import * as csvParser from 'csv-parser';
+
+const csvParser = require("csv-parser");
 
 const s3 = new AWS.S3();
 
 export async function handler(event: AWSLambda.S3Event): Promise<void> {
   console.log("event", event);
-  
+
   try {
     const record = event.Records[0];
     const bucketName = record.s3.bucket.name;
@@ -14,31 +15,31 @@ export async function handler(event: AWSLambda.S3Event): Promise<void> {
 
     console.log(`Object created in bucket: ${bucketName}, key: ${objectKey}`);
 
-    const s3ReadStream = s3.getObject({ Bucket: bucketName, Key: objectKey }).createReadStream();
-
+    const s3ReadStream = s3
+      .getObject({ Bucket: bucketName, Key: objectKey })
+      .createReadStream();
 
     const csvParserStream = s3ReadStream.pipe(csvParser());
 
     await new Promise<void>((resolve, reject) => {
-      csvParserStream.on('data', (data: any) => {
-        console.log('CSV Record:', data);
+      csvParserStream.on("data", (data: any) => {
+        console.log("CSV Record:", data);
       });
 
-      csvParserStream.on('end', () => {
-        console.log('CSV parsing finished');
-        resolve(); 
+      csvParserStream.on("end", () => {
+        console.log("CSV parsing finished");
+        resolve();
       });
 
-      csvParserStream.on('error', (err: Error) => {
-        console.error('Error parsing CSV:', err);
+      csvParserStream.on("error", (err: Error) => {
+        console.error("Error parsing CSV:", err);
         reject(err);
       });
     });
 
-    console.log('Lambda function execution finished successfully.');
-
+    console.log("Lambda function execution finished successfully.");
   } catch (err) {
-    console.error('Error processing S3 event:', err);
-    throw err; 
+    console.error("Error processing S3 event:", err);
+    throw err;
   }
 }
